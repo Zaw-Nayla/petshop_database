7ee278f0ce44a895a0aa5a42052e6f8ee32d7970
Zaw Nay La <zawnayla.znl@gmail.com>
2022-10-29 15:48:38 +0630

    i think i got it

--- db.py
+++ db.py
@@ -2,11 +2,17 @@ import imp
 from flask import Flask
 from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
-# import os
+import os
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:Zawnay2001@localhost/petshop'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL','postgresql://postgres:Zawnay2001@localhost/petshop')
+try:
+    proudURI = os.getenv('DATABASE_URL')
+    proudURI = proudURI.replace("postgres://","postgresql://")
+    app.config['SQLALCHEMY_DATABASE_URI'] = proudURI
+except:
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:Zawnay2001@localhost/petshop'
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = "She_didn't_love_U"
